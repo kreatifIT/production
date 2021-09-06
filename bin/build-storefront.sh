e3ddf9d90f6ffe6f3b17b16f8d906ee2ec898e14
Philipp Parth <p.parth@kreatif.it>
2021-09-06 10:00:45 +0200

    build-storefront.sh php fix

--- bin/build-storefront.sh
+++ bin/build-storefront.sh
@@ -6,9 +6,9 @@ export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname "$CWD")"}"
 STOREFRONT_ROOT="${STOREFRONT_ROOT:-"${PROJECT_ROOT}/vendor/shopware/storefront"}"
 
 # build storefront
-[[ ${CI} ]] || "${CWD}/console" bundle:dump
+[[ ${CI} ]] || php74 "${CWD}/console" bundle:dump
 npm --prefix "${STOREFRONT_ROOT}"/Resources/app/storefront clean-install
 node "${STOREFRONT_ROOT}"/Resources/app/storefront/copy-to-vendor.js
 npm --prefix "${STOREFRONT_ROOT}"/Resources/app/storefront run production
-[[ ${CI} ]] || "${CWD}/console" asset:install
-[[ ${CI} ]] || "${CWD}/console" theme:compile
+[[ ${CI} ]] || php74 "${CWD}/console" asset:install
+[[ ${CI} ]] || php74 "${CWD}/console" theme:compile
