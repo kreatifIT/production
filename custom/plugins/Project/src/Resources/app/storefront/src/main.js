040eb15dbf6fa9bc284235c166e97a20698860ed
Philipp Parth <philippparth@MacBook-Air-von-Philipp.local>
2022-05-12 16:15:26 +0200

    assets & dist files added

--- custom/plugins/Project/src/Resources/app/storefront/src/main.js
+++ custom/plugins/Project/src/Resources/app/storefront/src/main.js
@@ -1,6 +1,9 @@
+import ProjectPlugin from './project/project.plugin';
 import ProjectCartWidgetPlugin from './project-header/project-cart-widget.plugin';
 
 const PluginManager = window.PluginManager;
 // // Register your plugin via the existing PluginManager
-// PluginManager.override('AddToCart', ProjectAddToCartPlugin, '[data-add-to-cart]');
+PluginManager.register('ProjectPlugin', ProjectPlugin);
+
+// Override/Extend Plugin
 PluginManager.override('CartWidget', ProjectCartWidgetPlugin, '[data-cart-widget]');
---
+++ custom/plugins/Project/src/Resources/app/storefront/src/project/project.plugin.js
@@ -0,0 +1,6 @@
+import Plugin from 'src/plugin-system/plugin.class';
+
+export default class ProjectPlugin extends Plugin {
+    init() {
+    }
+}
